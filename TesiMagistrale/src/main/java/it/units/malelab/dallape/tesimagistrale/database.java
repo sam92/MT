60f8ecf5de75f70562ddd205ceff14941341a988
Samuele <Samuele@172.30.42.118>
2017-10-18 16:16:37 +0200

    insert db map stato

--- TesiMagistrale/src/main/java/it/units/malelab/dallape/tesimagistrale/database.java
+++ TesiMagistrale/src/main/java/it/units/malelab/dallape/tesimagistrale/database.java
@@ -16,8 +16,12 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.bson.Document;
 
 /**
@@ -34,7 +38,7 @@ public class database implements java.lang.AutoCloseable {
         MongoClientURI dbURI = new MongoClientURI(URI_MONGOLAB);
         mC = new MongoClient(dbURI);
         db = mC.getDatabase(dbURI.getDatabase());
-        System.out.println("Connected to:"+db.getName());
+        System.out.println("Connected to:" + db.getName());
         /*Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
         DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
         //c = DriverManager.getConnection("jdbc:derby://localhost:1527/sample/APP");
@@ -43,19 +47,21 @@ public class database implements java.lang.AutoCloseable {
     }
 
     public boolean collectionExist(String nameCollection) {
-        boolean exist=false;
-        MongoIterable<String> collectionNames= db.listCollectionNames();
-        MongoCursor<String> it= collectionNames.iterator();
-        while(it.hasNext()){
-            if(it.next().equalsIgnoreCase(nameCollection)) exist=true;
+        boolean exist = false;
+        MongoIterable<String> collectionNames = db.listCollectionNames();
+        MongoCursor<String> it = collectionNames.iterator();
+        while (it.hasNext()) {
+            if (it.next().equalsIgnoreCase(nameCollection)) {
+                exist = true;
+            }
         }
         return exist;
     }
 
     public void createCollection(String name) {
-        if(!collectionExist(name)){
-        db.createCollection(name);
-        System.out.println("Created table: "+name + " in db: "+db.getName());
+        if (!collectionExist(name)) {
+            db.createCollection(name);
+            System.out.println("Created table: " + name + " in db: " + db.getName());
         }
     }
 
@@ -68,12 +74,15 @@ public class database implements java.lang.AutoCloseable {
         }
         return b;
     }
-        public boolean updateSite(Site a, String collection) throws MongoException {
+
+    public boolean updateSite(Site a, String collection) throws MongoException {
         boolean b = collectionExist(collection);
         if (b) {
             //db.getCollection(collection).updateOne(new Document("entityId", "12").append("nameIdentity.dob",new Document("$exists",false)), new Document("$push", new Document("nameIdentity", new Document("fName", "1223").append("lName", "2222222") .append("dob", "00").append("address", "789"))));
             Document doc = db.getCollection(collection).findOneAndReplace(new Document("url_site", a.getUrl()), a.toDocument(), new FindOneAndReplaceOptions().upsert(true));
-        if(doc==null) b=false;
+            if (doc == null) {
+                b = false;
+            }
         }
         return b;
     }
@@ -92,29 +101,31 @@ public class database implements java.lang.AutoCloseable {
     }
 
     public boolean existADocumentWithThisUrlInSITES(String url) {
-        return getTheFirstDocumentWithThisKeyValue("url_site",url,"SITES")!=null;
+        return getTheFirstDocumentWithThisKeyValue("url_site", url, "SITES") != null;
     }
+
     public boolean existSiteInSTATE(String site) {
-        return getTheFirstDocumentWithThisKeyValue("site",site,"STATE_LIST_SITES")!=null;
+        return getTheFirstDocumentWithThisKeyValue("site", site, "STATE_LIST_SITES") != null;
     }
 
     //return the first Document with this url or null
     public Document getTheFirstDocumentWithThisKeyValue(String key, String value, String collection) {
         MongoCollection<Document> docs = db.getCollection(collection);
-        FindIterable<Document> lista=null;
-        if(value==null){
+        FindIterable<Document> lista = null;
+        if (value == null) {
             lista = docs.find();
-        }
-        else{
+        } else {
             lista = docs.find(new Document(key, value));
-    }
-        
+        }
+
         MongoCursor<Document> it = lista.iterator();
         Document a = null;
         if (it.hasNext()) {
             a = it.next();
-            if(value==null){
-                if(!a.containsKey(key)) a=null;
+            if (value == null) {
+                if (!a.containsKey(key)) {
+                    a = null;
+                }
             }
 
         }
@@ -133,10 +144,49 @@ public class database implements java.lang.AutoCloseable {
     public void close() {
         mC.close();
     }
-public long howMuchRemainsInSTATE(String task_id){
-    return db.getCollection("STATE_LIST_SITES").count(new Document("task_id",task_id));
-}
-public long howMuchRemainsInCollection(String key, Object value, String collection){
-    return db.getCollection(collection).count(new Document(key,value));
-}
+
+    public long howMuchRemainsInSTATE(String task_id) {
+        return db.getCollection("STATE_LIST_SITES").count(new Document("task_id", task_id));
+    }
+
+    public long howMuchRemainsInCollection(String key, Object value, String collection) {
+        return db.getCollection(collection).count(new Document(key, value));
+    }
+    public void insertValueMap(String key, Conditions con, String collection) {
+        if (!this.collectionExist(collection)) {
+            db.createCollection(collection);
+        }
+        Document document = db.getCollection(collection).find(new Document("hash", key)).first();
+        if (document == null) {
+            document = new Document("hash", key).append("value",con.toDocument());
+            db.getCollection(collection).insertOne(document);
+        }
+            
+    }
+
+    public Conditions getConditionFromMap(String task_id, String collection) {
+        if (!this.collectionExist(collection)) {
+            db.createCollection(collection);
+        }
+        Conditions con = null;
+        Document document = db.getCollection(collection).find(new Document("hash", task_id)).first();
+        if (document != null) {
+            con=Conditions.fromJSON(((Document) document.get("value")).toJson());
+        }
+        return con;
+    }
+
+    public Conditions deleteTaskIDFromMap(String task_id, String collection) {
+        if (!this.collectionExist(collection)) {
+            db.createCollection(collection);
+        }
+        Conditions con = null;
+        Document document = db.getCollection(collection).find(new Document("hash", task_id)).first();
+        if (document != null) {
+            // no test document, let's create one!
+            con=Conditions.fromJSON(((Document) document.get("value")).toJson());
+            db.getCollection(collection).deleteOne(document);
+        }
+        return con;
+    }
 }
