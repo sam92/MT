1570fb6b590d90f02e64634359c3a3fba48a1c7e
Samuele <Samuele@140.105.108.160>
2017-10-31 18:37:59 +0100

    take site from db if already present

--- TesiMagistrale/src/main/java/it/units/malelab/dallape/tesimagistrale/executeTest.java
+++ TesiMagistrale/src/main/java/it/units/malelab/dallape/tesimagistrale/executeTest.java
@@ -12,23 +12,17 @@ package it.units.malelab.dallape.tesimagistrale;
 import com.mongodb.MongoException;
 import java.util.List;
 import java.util.Map;
-import org.bson.Document;
 
 public class executeTest extends Thread {
 
     //private volatile boolean stop=false;
-    private final String COLLECTION_SITES;
-    private final String ACTUAL_STATE;
     private final Map<String, Boolean> progress;
     private final String task_id;
     private final Conditions con;
     private final List<String> whatTest;
     private final boolean reanalyze;
-    private final String stato;
 
-    public executeTest(String task_id, Conditions con, String nameStatoTable) {
-        this.COLLECTION_SITES = con.getNameCollection();
-        this.ACTUAL_STATE = con.getNameState();
+    public executeTest(String task_id, Conditions con) {
         this.task_id = task_id;
         if (!con.getTests().contains("")) {
             con.getTests().add("");
@@ -36,7 +30,6 @@ public class executeTest extends Thread {
         this.con = con;
         this.reanalyze = con.getReanalyze();
         this.whatTest = con.getTests();
-        stato = nameStatoTable;
         progress = con.getProgress();
     }
 
@@ -47,14 +40,14 @@ public class executeTest extends Thread {
                 synchronized (this) {
                     if (con.isStopped() || con.isPaused()) {
                         if (con.isPaused()) {
-                            db.updateValueMap(task_id, con, stato, false);
+                            db.updateValueMap(task_id, con, false);
                         }
                         break;
                     }
                     if (!db.existInSitesCollections(s) || reanalyze) {
                         if (!db.existInSTATE(s) && !s.isEmpty()) {
                             //inserisco tutti i sites nella lista dello stato.
-                            db.getMongoDB().getCollection(ACTUAL_STATE).insertOne(new Document("site", s).append("task_id", task_id));
+                            db.insertOneInState(s, task_id);
                         } else {
                             System.out.println("Already exist: " + s);
                         }
@@ -79,8 +72,15 @@ public class executeTest extends Thread {
                             break;
                         }
                         if ((!db.existInSitesCollections(s) || reanalyze) && !s.trim().isEmpty() && db.existInSTATE(s)) {
-                            Site site=new SiteImplementation(s);
-                            TestForm formTest = new TestFormImplementation(site,task_id);
+                            //se esiste gia nella mappa dei siti si pesca da là e si inseriscono cose nuove, poi si risostituisce il tutto
+                            Site site=db.getFromCollectionsSites(s);
+                            if(site==null){
+                                site = new SiteImplementation(s);
+                            }
+                            else{
+                                ((SiteImplementation)site).reScanRealUrl();
+                            }
+                            TestForm formTest = new TestFormImplementation(site, task_id);
                             if (!site.isUnreachable()) {
                                 if (whatTest.size() >= 6 && (whatTest.contains("wordpress") || whatTest.contains("joomla") || whatTest.contains("plone") || whatTest.contains("drupal") || whatTest.contains("typo3"))) {
                                     formTest.testAllCMS();
@@ -110,14 +110,14 @@ public class executeTest extends Thread {
                                 }
                                 formTest.start();
                                 site.insertTest(formTest);
-                                /*
-                                formTest.searchFormInThesePaths(formTest.listCMS());
-                                if (!(con.isPaused() || con.isStopped())) {
-                                    formTest.searchFormInLinkedPagesOfHomepage();
-                                }*/
-                                Test contactsTest= new TestContacts(site,formTest.getWebDriver() ,task_id);
+                                TestContacts contactsTest = new TestContactsImplementation(site, formTest.getWebDriver(), task_id);
+                                contactsTest.start();
                                 site.insertTest(contactsTest);
-                                formTest.quitWebDriver();
+                                TestWeight scad = new TestWeightImplementation(site, contactsTest.getWebDriver(), task_id);
+                                scad.start();
+                                site.insertTest(scad);
+                                scad.quitWebDriver();
+                                System.out.println(site.toJSONString());
                             } else {
                                 //is unreachable, inserisco in mappa cosi
                                 System.out.println("Is Unreachable: " + s);
@@ -135,6 +135,7 @@ public class executeTest extends Thread {
                                     }
                                 }
                             }
+                            //NON HA PIU MOLTO SENSO SCANSIONARE SE ESISTE GIA VISTO CHE LO PRELEVO SE ESISTE
                         } else if (db.existInSitesCollections(s) && !reanalyze) {
                             if (!(con.isPaused() || con.isStopped())) {
                                 Site alreadyExistent = db.getFromCollectionsSites(s);
@@ -145,23 +146,23 @@ public class executeTest extends Thread {
                         }
                         if (!(con.isPaused() || con.isStopped())) {
                             progress.replace(s, false, true);
-                            db.updateValueMap(task_id, con, stato, false);
+                            db.updateValueMap(task_id, con, false);
                         }
 
                     }
                 }
                 //tolgo dalla coda di questo task il doc perché è stato appena scansionato
-                db.getMongoDB().getCollection(ACTUAL_STATE).deleteOne(new Document("site", s).append("task_id", task_id));
+                db.deleteOneFromState(s, task_id);
                 //System.out.println("Cancello " + s);
             }
             if (!con.isPaused()) {
                 //ho fatto tutta la lista quindi posso rimuovere i siti con quel task dalla lista (nel caso in cui il thread sia stato killato prima di rimuovere tutto)
-                db.getMongoDB().getCollection(ACTUAL_STATE).deleteMany(new Document("task_id", task_id));
+                db.deleteManyFromState(task_id);
                 //rimuovo l'associazione tra task_id e thread tanto ho finito
-                db.deleteTaskIDFromMap(task_id, stato);
+                db.deleteTaskIDFromMap(task_id);
                 db.insertTaskID(task_id);
             } else {
-                db.updateValueMap(task_id, con, stato, false);
+                db.updateValueMap(task_id, con, false);
             }
 
         } catch (IllegalArgumentException | MongoException e) {
